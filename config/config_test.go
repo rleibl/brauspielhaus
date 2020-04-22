682210d11931d4a9bccd92584cb181b8c33a03b6
Robert Leibl <robert.leibl@haufe-lexware.com>
2020-04-22 13:47:42 +0200

    Add testing (config/config, models/beer)

---
+++ config/config_test.go
@@ -0,0 +1,10 @@
+
+package config
+
+import (
+	"testing"
+)
+
+func TestConfigInit(t *testing.T) {
+	_ = GetConfig()
+}
---
+++ models/beer_test.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"github.com/rleibl/brauspielhaus/config"
+	"testing"
+	"reflect"
+)
+
+func TestBeersLoad(t *testing.T) {
+
+	c := config.GetConfig()
+	b := LoadBeersFromJson(c.JsonPath)
+
+	if len(b) == 0 {
+		t.Errorf("No beers read from json")
+	}
+}
+
+func TestJson(t *testing.T) {
+
+	tmpl := BeerTemplate()
+	j := tmpl.ToJson()
+	tmpl2 := FromJson([]byte(j))
+
+	// FIXME
+	// Fix date handling in general.
+	// In this case, the original date has some kind of offset added to
+	// it, which makes the comparison fail.
+	//     tmpl:  Date: 2020-04-22 09:59:55.39177 +0200 CEST m=+0.001156567
+	//     tmpl2: Date: 2020-04-22 09:59:55.39177 +0200 CEST
+	tmpl2.Brewdate = tmpl.Brewdate
+
+	if ! reflect.DeepEqual(tmpl, tmpl2) {
+		tmpl.Print()
+		tmpl2.Print()
+		t.Errorf("Template -> JSON -> Template Failed")
+	}
+}
